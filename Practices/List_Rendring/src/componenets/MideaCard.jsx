c9ea86f8bf2f57e8e0a7f25eb073bc1a0ab456d8
sudheerkumarsharma <sudheerkumarsharma12@gmail.com>
2026-04-21 10:13:30 +0530

    add new code

--- Practices/List_Rendring/src/componenets/MideaCard.jsx
+++ Practices/List_Rendring/src/componenets/MideaCard.jsx
@@ -2,7 +2,7 @@ import jsonDatas from "./jsonData.json";
 
 const MideaCard = () => {
   return (
-    <div className=" w-full">
+    <div className=" w-full bg-black">
       {/* <div className="flex flex-col gap-10 p-10 ">
         {jsonDatas.map((items) => (
           <div className="flex items-center gap-[8rem]">
@@ -32,24 +32,32 @@ const MideaCard = () => {
       </div> */}
 
       <div className="flex flex-col gap-5 p-6">
-        {jsonDatas.map((items, index) => (
-          <div key={index} className="flex bg-[#282828]">
-            {/* Image */}
-            <div className="w-1/4 bg-fuchsia-300 flex items-center justify-center">
-              <img src={items.image} alt="" />
+        {jsonDatas.map((items, index) => {
+          return (
+            <div
+              key={index}
+              className="flex bg-[#282828] p-4 gap-4 rounded-2xl"
+            >
+              {/* Image */}
+              <div className="w-1/4 bg-fuchsia-300 flex items-center justify-center">
+                <img src={items.image} alt="" />
+              </div>
+              {/* ditail */}
+              <div className="flex-1 flex flex-col bg-green-300 justify-center">
+                <h1>{items.title}</h1>
+                <span>⭐ {items.rating}</span>
+                <span> | {items.reviews}</span>
+                {items.details.map((detail) => (
+                  <li className="ml-4">{detail}</li>
+                ))}
+              </div>
+              {/* pricing */}
+              <div className="bg-blue-300">
+                <p>{items.price}</p>
+              </div>
             </div>
-            {/* ditail */}
-            <div className="flex-1 bg-green-300">
-              <h1>{items.title}</h1>
-              <span>⭐ {items.rating}</span>
-              <span> | {items.reviews}</span>
-            </div>
-            {/* pricing */}
-            <div className="bg-blue-300">
-              <p>{items.price}</p>
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
